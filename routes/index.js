52f38e2a985f4398571144f80c361668e7853164
Gabriela <gabrielaliste@hotmail.com>
2023-10-26 11:09:32 -0300

    generado endpoint mail en particular

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Recuperatorio de Taller de Programación' });
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Practicando Simu Taller de Programación 2' });
 });
+
 
 module.exports = router;
